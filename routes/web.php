c56ed41f95e141da19422470468486011e6d127d
trisskmasarahh <trisukmasarahh@gmail.com>
2025-02-19 10:32:12 +0700

    lengkah 2

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,33 @@ Route::get('/about', function () {
     return 'Nama : Tri Sukma Sarah <br>
     Nim : 2341720051';
 });
+
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;
+    });
+
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+});
+
+
+Route::get('/articles/{id}', function 
+($id) { 
+    return 'Halaman Artkel dengan ID '.$id;
+});
+
+Route::get(	'/user/{name?}', function ($name=null) { 
+return 'Nama saya '.$name;
+});
+
+Route::get(	'/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/user/profile', function() { 
+    //
+    })->name('profile'); 
+    
